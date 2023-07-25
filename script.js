bfc9afac8e40145682ae7b45d12d3a7c418420e2
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2023-07-25 13:49:57 +0100

    added overflow for mobile view

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-// fetches json data from Cloudflare worker
+// // fetches json data from Cloudflare worker
 
-const fetchandlog = async () => {
-  try {
-    const response = await fetch('https://internetshutdowns.joeclark176.workers.dev/', {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/x-www-form-urlencoded'
-      })
-    });
+// const fetchandlog = async () => {
+//   try {
+//     const response = await fetch('https://internetshutdowns.joeclark176.workers.dev/', {
+//       method: 'GET',
+//       headers: new Headers({
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       })
+//     });
 
-    // Check if the request was successful
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${response.status}`);
-    }
+//     // Check if the request was successful
+//     if (!response.ok) {
+//       throw new Error(`Request failed with status ${response.status}`);
+//     }
 
-    // Parse the response body as JSON data
-    const jsonData = await response.json();
+//     // Parse the response body as JSON data
+//     const jsonData = await response.json();
 
-    // Print the JSON data
-    console.log(jsonData);
+//     // Print the JSON data
+//     console.log(jsonData);
 
-    // Return the JSON data
-    return jsonData;
-  } catch (error) {
-    console.error('Error fetching JSON:', error);
-    return null;
-  }
-};
+//     // Return the JSON data
+//     return jsonData;
+//   } catch (error) {
+//     console.error('Error fetching JSON:', error);
+//     return null;
+//   }
+// };
 
-fetchandlog();
+// fetchandlog();
 
-export default fetchandlog;
+// export default fetchandlog;
 
 function updateDateTime() {
   const currentDate = new Date()
   currentDate.setHours(0,0,0,0)
   const formattedDate = currentDate.toLocaleString(); // Customize the date format as needed
-  document.getElementById('updatedDate').textContent = "Last updated:" + formattedDate;
+  document.getElementById('updatedDate').textContent = "Last updated: " + formattedDate;
 }
 
 updateDateTime();
