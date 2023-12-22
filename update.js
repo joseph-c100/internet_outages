86670c92d6ea171f7568392cf56ff688af6bbe36
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2023-12-22 13:26:03 +0000

    style updates

---
+++ update.js
@@ -0,0 +1,16 @@
+function updateDateTime() {
+  const currentDate = new Date()
+  currentDate.setHours(0,0,0,0)
+  const formattedDate = currentDate.toLocaleString(); // Customize the date format as needed
+  document.getElementById('updatedDate').textContent = "Last updated: " + formattedDate;
+}
+
+updateDateTime();
+
+// Schedule the update to occur every 24 hours (in milliseconds)
+const updateInterval = 24 * 60 * 60 * 1000;
+setInterval(updateDateTime, updateInterval);
+
+
+
+
