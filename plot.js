b454d1022d0601a3f77b6d5969332edc709943f2
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2025-02-11 16:30:01 +0000

    possible fix for undefined name

--- plot.js
+++ plot.js
@@ -2,105 +2,107 @@ import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 async function fetchDataAndPlot() {
-try {
+  try {
     // Fetch JSON data for last week
-    const jsonDataURL7days = "https://internetshutdowns.joeclark176.workers.dev/";
+    const jsonDataURL7days =
+      "https://internetshutdowns.joeclark176.workers.dev/";
     const jsonData7days = await d3.json(jsonDataURL7days, d3.autoType);
 
     // Fetch JSON data for last 3 months
-    const jsonDataURL3months = "https://internetshutdowns3months.joeclark176.workers.dev/";
-    const jsonData3months = await d3.json(jsonDataURL3months, d3.autoType)
-
+    const jsonDataURL3months =
+      "https://internetshutdowns3months.joeclark176.workers.dev/";
+    const jsonData3months = await d3.json(jsonDataURL3months, d3.autoType);
 
     // Last week timeline
     // Convert startDate and endDate to Date objects
     jsonData7days.result.annotations.forEach((annotation) => {
-        annotation.startDate = new Date(annotation.startDate);
-        if (annotation.endDate) {
+      annotation.startDate = new Date(annotation.startDate);
+      if (annotation.endDate) {
         annotation.endDate = new Date(annotation.endDate);
-    }
+      }
     });
 
     // new current date if the outage is ongoing
-    const currentDate = new Date()
-    const colours = ["#505050", "#FB765A", "#8e80ff", "#2ba65c"]
+    const currentDate = new Date();
+    const colours = ["#505050", "#FB765A", "#8e80ff", "#2ba65c"];
 
     // Extracting start and end dates for the line chart
-    const dataForLineChart = jsonData7days.result.annotations.map((annotation) => ({
+    const dataForLineChart = jsonData7days.result.annotations.map(
+      (annotation) => ({
         startDate: annotation.startDate,
-        endDate:annotation.endDate || currentDate,
+        endDate: annotation.endDate || currentDate,
         OutageCause: annotation.outage.outageCause,
-        Country: annotation.locationsDetails[0].name,
-    }));
+        Country: annotation.locationsDetails[0]?.name || "",
+      })
+    );
 
     // Create the line chart
     const linePlot = Plot.plot({
-        color: {legend: true, range: colours},
-        height:400,
-        width:1000,
-        marginLeft: 80,
-        marginBottom:50,
-        inset: 20,
-        style: {fontSize: 12},
-        x: {
-            axis: "bottom",
-            grid: true,
-        },
-        y: {
-            grid: true,
-        },
-        marks: [
-            Plot.barX(dataForLineChart, { x1: "startDate", x2:"endDate", y: "Country", fill:"OutageCause"}),
-            Plot.frame({stroke:"red"}),
-        ],
-        });
+      color: { legend: true, range: colours },
+      height: 400,
+      width: 1000,
+      marginLeft: 80,
+      marginBottom: 50,
+      inset: 20,
+      style: { fontSize: 12 },
+      x: {
+        axis: "bottom",
+        grid: true,
+      },
+      y: {
+        grid: true,
+      },
+      marks: [
+        Plot.barX(dataForLineChart, {
+          x1: "startDate",
+          x2: "endDate",
+          y: "Country",
+          fill: "OutageCause",
+        }),
+        Plot.frame({ stroke: "red" }),
+      ],
+    });
 
-
-    const dataForCauseBarChart = jsonData3months.result.annotations.map((annotation) => ({
+    const dataForCauseBarChart = jsonData3months.result.annotations.map(
+      (annotation) => ({
         OutageCause: annotation.outage.outageCause,
-        Country: annotation.locationsDetails[0].name,
-    }));
+        Country: annotation.locationsDetails[0]?.name || "",
+      })
+    );
 
     // Create dot chart for last 3 months
     const causeBarChart = Plot.plot({
-        color: { legend: false },
-        width: 1000,
-        height: 400,
-        insetLeft: 20,
-        marginLeft: 40,
-        marginBottom: 40,
-        insetBottom: 5,
-        marginBottom: 110,
-        marks: [
-            Plot.gridY({
-                strokeDasharray: "0.75,2", // dashed
-                strokeOpacity: 1 // opaque
-            }),
-            Plot.barY(
-                dataForCauseBarChart,
-                Plot.groupX(
-                    { y: "count" },
-                    { x: "OutageCause"},
-                ),
-            ),
-            Plot.axisX({anchor: "bottom", label: null, tickRotate: -45}),
-            Plot.ruleY([0])
-            ]
-    })
-
+      color: { legend: false },
+      width: 1000,
+      height: 400,
+      insetLeft: 20,
+      marginLeft: 40,
+      marginBottom: 40,
+      insetBottom: 5,
+      marginBottom: 110,
+      marks: [
+        Plot.gridY({
+          strokeDasharray: "0.75,2", // dashed
+          strokeOpacity: 1, // opaque
+        }),
+        Plot.barY(
+          dataForCauseBarChart,
+          Plot.groupX({ y: "count" }, { x: "OutageCause" })
+        ),
+        Plot.axisX({ anchor: "bottom", label: null, tickRotate: -45 }),
+        Plot.ruleY([0]),
+      ],
+    });
 
     // append plots to their div
     const outageTimelineDiv = document.querySelector("#outageTimeline");
     outageTimelineDiv.append(linePlot);
 
-
     const outageCauseDiv = document.querySelector("#outagesCauses");
     outageCauseDiv.append(causeBarChart);
-
-
-} catch (error) {
+  } catch (error) {
     console.error("Error fetching JSON:", error);
-}
+  }
 }
 
 fetchDataAndPlot();
--- table.js
+++ table.js
@@ -2,111 +2,142 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 // function to populate 7 days table
 async function fetchData() {
-    try {
-        // Fetch JSON data from the provided link
-        const jsonData7days = await d3.json('https://internetshutdowns.joeclark176.workers.dev/', d3.autoType);
-        const jsonData3months = await d3.json('https://internetshutdowns3months.joeclark176.workers.dev/', d3.autoType);
+  try {
+    // Fetch JSON data from the provided link
+    const jsonData7days = await d3.json(
+      "https://internetshutdowns.joeclark176.workers.dev/",
+      d3.autoType
+    );
+    const jsonData3months = await d3.json(
+      "https://internetshutdowns3months.joeclark176.workers.dev/",
+      d3.autoType
+    );
 
-        const currentDate = new Date();
+    const currentDate = new Date();
 
-        // Define column headers for both tables
-        const columnHeaders = ['Country', 'Start Date', 'End Date', 'Outage Cause', 'Scope', 'Description', 'ASN'];
+    // Define column headers for both tables
+    const columnHeaders = [
+      "Country",
+      "Start Date",
+      "End Date",
+      "Outage Cause",
+      "Scope",
+      "Description",
+      "ASN",
+    ];
 
-        // Create a table header for 7 days table
-        const tableHeader7days = d3.select('#outageTableWeek')
-            .append('thead')
-            .append('tr')
-            .selectAll('th')
-            .data(columnHeaders)
-            .enter()
-            .append('th')
-            .text(d => d);
+    // Create a table header for 7 days table
+    const tableHeader7days = d3
+      .select("#outageTableWeek")
+      .append("thead")
+      .append("tr")
+      .selectAll("th")
+      .data(columnHeaders)
+      .enter()
+      .append("th")
+      .text((d) => d);
 
-        // map data for 7 days table
-        const dataForTable7days = jsonData7days.result.annotations.map((annotation) => ({
-            startDate: annotation.startDate,
-            endDate: annotation.endDate || currentDate,
-            OutageCause: annotation.outage.outageCause,
-            Country: annotation.locationsDetails[0].name,
-            Scope: annotation.scope,
-            description: annotation.description,
-            asns: annotation.asns
-        }));
+    // map data for 7 days table
+    const dataForTable7days = jsonData7days.result.annotations.map(
+      (annotation) => ({
+        startDate: annotation.startDate,
+        endDate: annotation.endDate || currentDate,
+        OutageCause: annotation.outage.outageCause,
+        Country: annotation.locationsDetails[0].name,
+        Scope: annotation.scope,
+        description: annotation.description,
+        asns: annotation.asns,
+      })
+    );
 
-        // Create table rows with the data
-        const rows7days = d3.select('#outageTableWeek')
-            .selectAll('tr.row7days')
-            .data(dataForTable7days)
-            .enter()
-            .append('tr')
-            .attr('class', 'row7days');
+    // Create table rows with the data
+    const rows7days = d3
+      .select("#outageTableWeek")
+      .selectAll("tr.row7days")
+      .data(dataForTable7days)
+      .enter()
+      .append("tr")
+      .attr("class", "row7days");
 
-        // Create table cells and populate with data
-        rows7days.append('td').text(d => d.Country);
-        rows7days.append('td').text(d => d.startDate);
-        rows7days.append('td').text(d => (d.endDate !== currentDate ? d.endDate : 'null (Possibly ongoing)'));
-        rows7days.append('td').text(d => d.OutageCause);
-        rows7days.append('td').text(d => d.Scope);
-        rows7days.append('td').text(d => d.description);
+    // Create table cells and populate with data
+    rows7days.append("td").text((d) => d.Country);
+    rows7days.append("td").text((d) => d.startDate);
+    rows7days
+      .append("td")
+      .text((d) =>
+        d.endDate !== currentDate ? d.endDate : "null (Possibly ongoing)"
+      );
+    rows7days.append("td").text((d) => d.OutageCause);
+    rows7days.append("td").text((d) => d.Scope);
+    rows7days.append("td").text((d) => d.description);
 
-        // Append the ASN names using <span> tags
-        rows7days.append('td')
-            .attr('class', 'asn-cell')
-            .selectAll('span')
-            .data(d => (d.asns ? d.asns : []))
-            .enter()
-            .append('span')
-            .text(d => d);
+    // Append the ASN names using <span> tags
+    rows7days
+      .append("td")
+      .attr("class", "asn-cell")
+      .selectAll("span")
+      .data((d) => (d.asns ? d.asns : []))
+      .enter()
+      .append("span")
+      .text((d) => d);
 
-        // Create a table header for 3 months table
-        const tableHeader3months = d3.select('#outageTableMonths')
-            .append('thead')
-            .append('tr')
-            .selectAll('th')
-            .data(columnHeaders)
-            .enter()
-            .append('th')
-            .text(d => d);
+    // Create a table header for 3 months table
+    const tableHeader3months = d3
+      .select("#outageTableMonths")
+      .append("thead")
+      .append("tr")
+      .selectAll("th")
+      .data(columnHeaders)
+      .enter()
+      .append("th")
+      .text((d) => d);
 
-        // map data for 3 months table
-        const dataForTable3months = jsonData3months.result.annotations.map((annotation) => ({
-            startDate: annotation.startDate,
-            endDate: annotation.endDate || currentDate,
-            OutageCause: annotation.outage.outageCause,
-            Country: annotation.locationsDetails[0].name,
-            Scope: annotation.scope,
-            description: annotation.description,
-            asns: annotation.asns
-        }));
+    // map data for 3 months table
+    const dataForTable3months = jsonData3months.result.annotations.map(
+      (annotation) => ({
+        startDate: annotation.startDate,
+        endDate: annotation.endDate || currentDate,
+        OutageCause: annotation.outage.outageCause,
+        Country: annotation.locationsDetails[0]?.name || "",
+        Scope: annotation.scope,
+        description: annotation.description,
+        asns: annotation.asns,
+      })
+    );
 
-        // Create table rows with the data
-        const rows3months = d3.select('#outageTableMonths')
-            .selectAll('tr.row3months')
-            .data(dataForTable3months)
-            .enter()
-            .append('tr')
-            .attr('class', 'row3months');
+    // Create table rows with the data
+    const rows3months = d3
+      .select("#outageTableMonths")
+      .selectAll("tr.row3months")
+      .data(dataForTable3months)
+      .enter()
+      .append("tr")
+      .attr("class", "row3months");
 
-        // Create table cells and populate with data
-        rows3months.append('td').text(d => d.Country);
-        rows3months.append('td').text(d => d.startDate);
-        rows3months.append('td').text(d => (d.endDate !== currentDate ? d.endDate : 'null (Possibly ongoing)'));
-        rows3months.append('td').text(d => d.OutageCause);
-        rows3months.append('td').text(d => d.Scope);
-        rows3months.append('td').text(d => d.description);
+    // Create table cells and populate with data
+    rows3months.append("td").text((d) => d.Country);
+    rows3months.append("td").text((d) => d.startDate);
+    rows3months
+      .append("td")
+      .text((d) =>
+        d.endDate !== currentDate ? d.endDate : "null (Possibly ongoing)"
+      );
+    rows3months.append("td").text((d) => d.OutageCause);
+    rows3months.append("td").text((d) => d.Scope);
+    rows3months.append("td").text((d) => d.description);
 
-        // Append the ASN names using <span> tags
-        rows3months.append('td')
-            .attr('class', 'asn-cell')
-            .selectAll('span')
-            .data(d => (d.asns ? d.asns : []))
-            .enter()
-            .append('span')
-            .text(d => d);
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+    // Append the ASN names using <span> tags
+    rows3months
+      .append("td")
+      .attr("class", "asn-cell")
+      .selectAll("span")
+      .data((d) => (d.asns ? d.asns : []))
+      .enter()
+      .append("span")
+      .text((d) => d);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
 fetchData();
